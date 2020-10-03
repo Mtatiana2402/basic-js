ca89770bf33cbd39d4060bfad63cf8f8d0159878
Tatiana Volkova <mtatiana2402@gmail.com>
2020-10-03 18:25:54 +0200

    Add dream-team.js

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,18 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (Array.isArray(members)) {
+    var newTeam = [];
+        for (i=0; i<members.length; i++) {
+            if (typeof members[i] === 'string') {
+            var str = members[i];
+            newTeam[i] = str.split('').filter(el => el !== ' ').shift().toUpperCase();
+            }
+        else {
+            newTeam[i] = "";
+        }
+    }
+    return newTeam = newTeam.filter(el => el !=="").sort().join('');    
+    }
+  else return false;
 };
